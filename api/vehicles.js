cd79de96a025607a91dad9775899a05fcfce2029
Eddiebaj <80553898+Eddiebaj@users.noreply.github.com>
2026-03-06 18:29:06 -0500

    Update vehicles.js

--- api/vehicles.js
+++ api/vehicles.js
@@ -55,7 +55,7 @@ module.exports = async (req, res) => {
 
   try {
     const now = Math.floor(Date.now() / 1000);
-    const stopsMap = getStopsMap();
+    onst stopsMap = await getStopsMap();
     const stopsCount = Object.keys(stopsMap).length;
 
     // 1. Fetch TripUpdates
